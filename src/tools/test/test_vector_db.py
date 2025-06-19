62eaead3061836abbb064dc33d8383e38eef8e1f
baolongnguyenmac <baolongnguyen.mac@gmail.com>
2025-06-19 10:02:51 +0700

    vector db

---
+++ src/tools/test/test_vector_db.py
@@ -0,0 +1,44 @@
+from tools.vector_db import VectorDB
+from langchain_core.documents.base import Document
+
+from shutil import rmtree
+
+from dotenv import load_dotenv
+load_dotenv()
+
+class TestVectorDB:
+    persist_directory='./src/tools/test/assets'
+    collection_name='test'
+    filepath = './data/2021 survey FL.pdf'
+
+    vector_db = VectorDB(
+        collection_name=collection_name,
+        persist_directory=persist_directory
+    )
+
+    def test_get_chunk(self):
+        # filepath = './data/2021 survey FL.pdf'
+        docs = self.vector_db.get_chunk(self.filepath)
+
+        assert type(docs) is list
+        assert type(docs[0]) is Document
+
+    def test_add_doc(self):
+        # filepath = './data/2021 survey FL.pdf'
+        self.vector_db.add_doc(self.filepath)
+
+    def test_similarity_search(self):
+        query = 'Federated learning'
+        k = 10
+        relevant_docs = self.vector_db.similarity_search(query, k)
+
+        assert len(relevant_docs) == k
+        assert type(relevant_docs) is list
+        assert type(relevant_docs[0]) is Document
+
+    def test_remove_collection(self):
+        rmtree(self.persist_directory)
+
+if __name__=='__main__':
+    tester = TestVectorDB()
+    tester.test_remove_collection()
---
+++ src/tools/vector_db.py
@@ -0,0 +1,62 @@
+# chunking
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter as Splitter
+
+# embedding and storing
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_chroma.vectorstores import Chroma
+from langchain_core.documents.base import Document
+
+# tool
+from langchain_core.tools import tool
+
+class VectorDB:
+    def __init__(self, collection_name:str, persist_directory:str) -> None:
+        self.vector_store:Chroma = Chroma(
+            collection_name=collection_name,
+            embedding_function=GoogleGenerativeAIEmbeddings(model="models/text-embedding-004"),
+            persist_directory=persist_directory
+        )
+
+    def get_chunk(self, filepath:str) -> list[Document]:
+        loader:PyPDFLoader = PyPDFLoader(file_path=filepath)
+        doc:list[Document] = loader.load()
+
+        text_splitter:Splitter = Splitter(chunk_size=1000, chunk_overlap=200, add_start_index=True)
+        chunks:list[Document] = text_splitter.split_documents(doc)
+        return chunks
+
+    def add_doc(self, filepath:str) -> None:
+        chunks:list[Document] = self.get_chunk(filepath)
+        # TODO: kiếm cách tích hợp id vào đây bằng hàm băm
+        self.vector_store.add_documents(chunks)
+
+    def similarity_search(self, query:str, k:int) -> list[Document]:
+        return self.vector_store.similarity_search(query=query, k=k)
+
+    def get_db_searcher(self, k:int):
+        """return a semantic search tool that searches for relevant documents in vector db with a given query
+
+        Args:
+            k (int): number of return documents
+
+        Returns:
+            retrieve: a function
+        """
+
+        @tool(response_format='content_and_artifact')
+        def retrieve(query:str) -> tuple[str, list[Document]]:
+            """Search for relevant contents in vector database with a given query
+
+            Args:
+                query (str): Query
+
+            Returns:
+                content (str): A string of relevant content
+                relevant_docs (list[Document]): a list of relevant documents
+            """
+            relevant_docs = self.vector_store.similarity_search(query=query, k=k)
+            content = '\n\n'.join([doc.page_content for doc in relevant_docs])
+            return content, relevant_docs
+
+        return retrieve
